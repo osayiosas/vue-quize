881b25c0d704ae34a6f39702c2667663ee13e3ba
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2024-06-22 18:02:24 +0100

    vue-quiz: building a quize app with vue

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,32 @@
+<template>
+     <header>
+      <h4>Question 1/3</h4>
+      <div class="bar">
+        <div class="completion"></div>
+      </div>
+    </header>
+</template>
+
+
+<style scoped>
+header {
+  margin-top: 20px;
+}
+
+header h4 {
+  font-weight: bold;
+  font-size: 40px;
+}
+
+.bar {
+  width: 300px;
+  height: 50px;
+  border: 3px solid beige;
+}
+
+.completion {
+  width: 0%;
+  height: 100%;
+  background: beige;
+}
+</style>
